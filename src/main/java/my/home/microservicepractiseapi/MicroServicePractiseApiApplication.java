f663e56c19e045221e47b70f63803e51e6c3b841
ashilov87 <it-shilov@yandex.ru>
2024-07-14 20:14:21 +0300

    First Commit: gradle+application.yaml+datasource

---
+++ src/main/java/my/home/microservicepractiseapi/MicroServicePractiseApiApplication.java
@@ -0,0 +1,13 @@
+package my.home.microservicepractiseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicePractiseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServicePractiseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/home/microservicepractiseapi/MicroServicePractiseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package my.home.microservicepractiseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServicePractiseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
